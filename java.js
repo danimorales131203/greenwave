a9b9a240ee6c4f6621fcf8b1efc8dae3a2af1501
Daniel Morales <adanielmorales0@gmail.com>
2026-04-14 17:49:45 -0700

    Pantalla de inicio setup

---
+++ java.js
@@ -0,0 +1,10 @@
+const loginBtn = document.getElementById("loginBtn");
+const registerBtn = document.getElementById("registerBtn");
+
+loginBtn.addEventListener("click", () => {
+    alert("Aquí después conectaremos la pantalla de inicio de sesión.");
+});
+
+registerBtn.addEventListener("click", () => {
+    alert("Aquí después conectaremos la pantalla de registro.");
+});
